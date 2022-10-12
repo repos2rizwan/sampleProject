a09946ba46a3cc00a183d1a6cbe8f9bab4a8d7f4
repos2rizwan <ranoorie@gmail.com>
2022-10-13 03:30:51 +0530

    Added a basic pattern print B file

--- B.java
+++ B.java
@@ -1,5 +1,13 @@
 class A{
 public static void main(String[] args){
 System.out.println("hello world");
+for (int i= 0; i<5;i++)
+{
+    for (int j=0; j<=i;j++)
+    {
+        System.out.print("*");
+    }
+    System.out.println("");
+}
 }
 }
